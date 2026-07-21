3d589ef4bf120135bb69e6a90edd3c78484027fe
basketmanyork-design <basketmanyork@gmail.com>
2026-07-21 12:01:01 +0800

    fix(studio): assign stable IDs to all structure nodes

--- studio/deskStructure.ts
+++ studio/deskStructure.ts
@@ -6,6 +6,7 @@ import {
 
 export const deskStructure: StructureResolver = (S) =>
   S.list()
+    .id('poxiolCms')
     .title('POXIOL CMS')
     .items([
       S.listItem()
@@ -25,56 +26,108 @@ export const deskStructure: StructureResolver = (S) =>
       S.divider(),
 
       S.listItem()
+        .id('siteSettings')
         .title('网站全局设置')
         .icon(EarthGlobeIcon)
-        .child(S.document().schemaType('siteSettings').documentId('siteSettings')),
+        .child(
+          S.document()
+            .id('siteSettingsEditor')
+            .schemaType('siteSettings')
+            .documentId('siteSettings')
+        ),
 
       S.listItem()
+        .id('procurementStandards')
         .title('采购参数标准')
         .icon(CogIcon)
-        .child(S.document().schemaType('procurementStandards').documentId('procurementStandards')),
+        .child(
+          S.document()
+            .id('procurementStandardsEditor')
+            .schemaType('procurementStandards')
+            .documentId('procurementStandards')
+        ),
 
       S.listItem()
+        .id('navigationFooter')
         .title('导航与Footer')
         .icon(DocumentsIcon)
-        .child(S.list().title('建设中')),
+        .child(
+          S.list()
+            .id('navigationFooterPlaceholder')
+            .title('建设中')
+        ),
 
       S.divider(),
 
       S.listItem()
+        .id('productCategories')
         .title('产品分类')
         .icon(TagIcon)
-        .child(S.documentTypeList('productCategory').title('产品分类')),
+        .child(
+          S.documentTypeList('productCategory')
+            .id('productCategoryList')
+            .title('产品分类')
+        ),
 
       S.listItem()
+        .id('products')
         .title('产品管理')
         .icon(PackageIcon)
-        .child(S.documentTypeList('product').title('产品')),
+        .child(
+          S.documentTypeList('product')
+            .id('productList')
+            .title('产品')
+        ),
 
       S.listItem()
+        .id('caseStudies')
         .title('客户案例')
         .icon(CaseIcon)
-        .child(S.documentTypeList('caseStudy').title('客户案例')),
+        .child(
+          S.documentTypeList('caseStudy')
+            .id('caseStudyList')
+            .title('客户案例')
+        ),
 
       S.listItem()
+        .id('faqKnowledgeBase')
         .title('FAQ 知识库')
         .icon(HelpCircleIcon)
-        .child(S.documentTypeList('faqItem').title('FAQ')),
+        .child(
+          S.documentTypeList('faqItem')
+            .id('faqItemList')
+            .title('FAQ')
+        ),
 
       S.listItem()
+        .id('articles')
         .title('文章与指南')
         .icon(DocumentIcon)
-        .child(S.documentTypeList('article').title('文章与指南')),
+        .child(
+          S.documentTypeList('article')
+            .id('articleList')
+            .title('文章与指南')
+        ),
 
       S.divider(),
 
       S.listItem()
+        .id('authors')
         .title('作者管理')
         .icon(UserIcon)
-        .child(S.documentTypeList('author').title('作者')),
+        .child(
+          S.documentTypeList('author')
+            .id('authorList')
+            .title('作者')
+        ),
 
       S.listItem()
+        .id('redirectRules')
         .title('301 重定向')
         .icon(DocumentsIcon)
-        .child(S.list().title('建设中')),
+        .child(
+          S.list()
+            .id('redirectRulesPlaceholder')
+            .title('建设中')
+        ),
     ])
